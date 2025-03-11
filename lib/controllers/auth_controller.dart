c4ab882d893e16cb7cf913d46cb67a66626d017c
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-03-11 17:43:27 +0100

    creation of auth controller

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,64 @@
+// controllers/auth_controller.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseStorage storage = FirebaseStorage.instance;
+
+  var isLoading = false.obs;
+  var selectedRole = ''.obs;
+
+  final List<String> userRoles = [
+    "Reporter",
+    "Cameraman",
+    "Assignment Editor",
+    "Head of Department",
+    "Admin"
+  ];
+
+  Future<void> signUp(String email, String password, String role) async {
+    try {
+      isLoading(true);
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await firestore.collection("users").doc(userCredential.user!.uid).set({
+        "uid": userCredential.user!.uid,
+        "email": email,
+        "role": role,
+        "profilePic": "",
+      });
+
+      Get.toNamed("/profile-update");
+    } catch (e) {
+      Get.snackbar("Error", e.toString());
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  Future<void> uploadProfilePicture(File imageFile) async {
+    try {
+      String uid = auth.currentUser!.uid;
+      Reference ref = storage.ref().child("profile_pics/$uid.jpg");
+      await ref.putFile(imageFile);
+      String downloadUrl = await ref.getDownloadURL();
+
+      await firestore
+          .collection("users")
+          .doc(uid)
+          .update({"profilePic": downloadUrl});
+    } catch (e) {
+      Get.snackbar("Error", "Failed to upload image");
+    }
+  }
+}
