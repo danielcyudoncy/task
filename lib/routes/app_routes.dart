9369c17284020cdce07fe27b190fa3b02369a1c5
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-03-16 00:23:04 +0100

    updated the task assignment screen

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,10 +2,12 @@
 import 'package:get/get.dart';
 import 'package:task/views/profile_update_screen.dart';
 import 'package:task/views/signup_screen.dart';
+import 'package:task/views/task_assignment_screen.dart';
 
 class AppRoutes {
   static final routes = [
     GetPage(name: "/signup", page: () => SignUpScreen()),
+    GetPage(name: "/assignTask", page: () => TaskAssignmentScreen()),
     GetPage(name: "/profile-update", page: () => const ProfileUpdateScreen()),
   ];
 }
