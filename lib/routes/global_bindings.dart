b9d7d5ef2ad414cab7202929ddaf7290c081b341
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-04-19 18:53:11 +0100

    updated the code

--- lib/routes/global_bindings.dart
+++ lib/routes/global_bindings.dart
@@ -1,16 +1,23 @@
 // routes/global_bindings.dart
 import 'package:get/get.dart';
+import 'package:task/controllers/notification_controller.dart';
+import 'package:task/service/firebase_service.dart';
 import '../controllers/auth_controller.dart';
 import '../controllers/admin_controller.dart';
 
 class GlobalBindings extends Bindings {
   @override
   void dependencies() {
+
+    Get.put<AuthController>(AuthController(), permanent: true);
+    Get.put<AdminController>(AdminController(), permanent: true);
     // Make AuthController permanent so it doesn't get disposed
+    Get.lazyPut<FirebaseService>(() => FirebaseService(), fenix: true);
     Get.lazyPut<AuthController>(() => AuthController(), fenix: true);
     Get.lazyPut<AdminController>(() => AdminController(), fenix: true);
 
     // Other controllers
     Get.lazyPut<AdminController>(() => AdminController());
+    Get.lazyPut<NotificationController>(() => NotificationController());
   }
 }
