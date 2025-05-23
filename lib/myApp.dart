a85ee2e366b60790100eb737c6b70eae76e5c8d8
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-05-23 22:32:49 +0100

    updated

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Don't call Get.find here!
+    // Don't call Get.find here
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Assignment Logging App',
