a1d6d46549a8a6e2932c476239555c090a7688b7
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-05-23 22:31:45 +0100

    updated

--- lib/routes/global_bindings.dart
+++ lib/routes/global_bindings.dart
@@ -2,6 +2,7 @@
 import 'package:get/get.dart';
 import 'package:task/controllers/manage_users_controller.dart';
 import 'package:task/controllers/notification_controller.dart';
+import 'package:task/controllers/settings_controller.dart';
 import 'package:task/service/firebase_service.dart';
 import '../controllers/auth_controller.dart';
 import '../controllers/admin_controller.dart';
@@ -12,6 +13,7 @@ class GlobalBindings extends Bindings {
     // Permanent controllers
     Get.put<AuthController>(AuthController(), permanent: true);
     Get.put<AdminController>(AdminController(), permanent: true);
+    Get.put(SettingsController(), permanent: true);
 
     // Lazy-loaded controllers and services
     Get.lazyPut<FirebaseService>(() => FirebaseService(), fenix: true);
