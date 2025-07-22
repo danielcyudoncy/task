9afdbc6823040ef8e5e252aa06410a13528cf815
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-07-22 03:23:39 +0100

    update on isar implementation

--- lib/routes/global_bindings.dart
+++ lib/routes/global_bindings.dart
@@ -1,12 +1,17 @@
 // routes/global_bindings.dart
 
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
 
 class GlobalBindings extends Bindings {
+  final Isar isar;
+  GlobalBindings(this.isar);
+
   @override
   void dependencies() {
     // This method is now empty.
     // All global, permanent controllers are initialized once in bootstrap.dart.
     // This file can still be used for screen-specific bindings if needed later.
+    // Removed IsarTaskService registration; now handled globally in bootstrapApp.
   }
 }
