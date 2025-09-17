32a7ed4ffd1a044d15abbef72ee37c8f9fe9e3d3
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-09-17 20:05:43 +0100

    updated the fringerprint unlock

--- android/app/src/main/kotlin/com/example/task/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/task/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.task
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
