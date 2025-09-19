ae5491046fb7ad3fdf1fbf9f2f6d9bdc25496a64
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2025-09-19 21:58:31 +0100

    fix the obsolate code

--- lib/widgets/user_dashboard_cards_widget.dart
+++ lib/widgets/user_dashboard_cards_widget.dart
@@ -251,7 +251,7 @@ class _DashboardGridCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(24),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.15),
+              color: Colors.black.withValues(alpha:0.15),
               blurRadius: 12,
               offset: const Offset(0, 6),
             ),
